19ba60341cc73637e5afbf01399cfe0a290e073c
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-12-14 21:51:30 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -1,16 +1,26 @@
 #include <bits/stdc++.h>
-
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ar array
+
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	cout << ((n/k)%k==0?"NO":"YES") << "\n";
+}
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-  int tt;
-  cin >> tt;
-  while (tt--) {
-    long long n, k;
-    cin >> n >> k;
-    cout << (((n / k) % k == 0) ? "NO" : "YES") << '\n';
-  }
-  return 0;
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int t;
+	cin >> t;
+	while(t--)
+		solve();
 }
